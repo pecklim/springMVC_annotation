7b9dad4ac774609c815b1534ecd8651e3894697e
peck_lim <peck_lim@163.com>
2021-12-02 16:06:34 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello,git4!");
         System.out.println("master,git5!");
         System.out.println("hot- fix");
+        System.out.println("push test");
     }
 }
