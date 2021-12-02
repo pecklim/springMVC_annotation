af3944895a2532ebe254f1c1844d204699820b13
pecklim <95363437+pecklim@users.noreply.github.com>
2021-12-02 16:12:05 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master,git5!");
         System.out.println("hot- fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
